a1cc7e40567cd6eb4d889e12b0d65bdc57f374bd
Dikki AP <dikki.1710@gmail.com>
2023-03-26 14:14:19 +0700

    Changing sayHello function

--- lib/src/say_hello.dart
+++ lib/src/say_hello.dart
@@ -1,3 +1,3 @@
-String sayHello(String name) => 'Hello $name';
+String sayHello(String name) => 'Hello $name, nice to see you';
 String sayMessage(String sender, String message, String receiver) =>
     '$sender said $message to $receiver';
